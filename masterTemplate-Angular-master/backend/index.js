d629a617e4d62c3524df48ca63625dacd0bdba57
m-triple-m <triplem656@gmail.com>
2021-05-18 12:08:30 +0530

    ..

--- masterTemplate-Angular-master/backend/index.js
+++ masterTemplate-Angular-master/backend/index.js
@@ -9,7 +9,7 @@ const utilRouter = require('./routers/util');
 const cors = require('cors');
 
 var corsOptions = {
-    origin: 'http://192.168.43.104:4200',
+    origin: 'http://192.168.43.91:4200',
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
@@ -23,6 +23,6 @@ app.use('/util', utilRouter);
 
 app.use(express.static('./uploads'))
 
-app.listen(port, '192.168.43.104', () => {
+app.listen(port, '192.168.43.91', () => {
     console.log('Hurray!!!!! server started on port ' + port);
 });
