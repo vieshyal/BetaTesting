3ab95a120ace04fa16509965324f49d4a2c78173
vieshyal <vieshyal@gmail.com>
2021-05-31 11:57:28 +0530

    ...

--- masterTemplate-Angular-master/src/config.ts
+++ masterTemplate-Angular-master/src/config.ts
@@ -1,9 +1,5 @@
 export const app_config = {
-<<<<<<< HEAD
   api_url: 'http://192.168.43.91:3000',
-=======
-  api_url: 'http://192.168.43.104:3000',
->>>>>>> 49e1015e5f643a0a2af6813f6aa0474668bb5785
   OAuthID:
     '436370605107-a1a87949khjquees4o8m7cjeq3mpiu8b.apps.googleusercontent.com',
   OAuthSecret: 'TU4yHdqdofILkOsw-grZyW6S',
